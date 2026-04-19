63327da294a62432a4998ea9db90deb4ea3cdcd3
Dipu <153580850+21tuckdipu@users.noreply.github.com>
2026-04-19 22:38:41 +0530

    practise-questions

---
+++ practise/functionstudy.js
@@ -0,0 +1,30 @@
+
+//import * as assert from 'assert/strict';
+
+//arrow function
+const add2 = (a,b) => {return a+b};
+console.log(add2(7,8));
+
+//function 
+function invite(senderMsg){
+    return 'Hello World from '+ senderMsg;
+}
+console.log(invite('Dipu'));
+
+//condition statement
+let x = 13;
+if (x<1){
+    x=-x
+}
+//console.log(x);
+//assert.equal(3+4,7);
+
+// let i = 0
+// while (x<20){
+//     i =i+ 1
+//     console.log(i);
+//     if (i==12){
+//         break;
+//     }
+    
+// }
