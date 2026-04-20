66f050f7d70f3b7b880c815520a629d051f48fa0
Dipu <153580850+21tuckdipu@users.noreply.github.com>
2026-04-20 23:12:15 +0530

    updated the map.js

--- loops/map.js
+++ loops/map.js
@@ -1,8 +1,30 @@
-const myNum = [1,2,3,4,5,6,7,8,9,10]
+"use strict";
+//Maps Basic
+const userGuide = new Map();
+userGuide.set("backpack",["cloth","toys","foods"]);
+userGuide.set("communication","tabel");
 
+//looping through the map
+for(const [key,value] of userGuide){
+    console.log(key,value)
+}
+
+//get method
+console.log(userGuide.get("backpack"));
+
+console.log("<!-------------Map-Methods----------------");
+console.log(userGuide.set("backpack","OK"));
+console.log(userGuide.size);
+console.log(userGuide.delete("backpack"));
+console.log(userGuide.has("backpack"));
+console.log(userGuide.clear());
+
+console.log("<!----------------------------");
+
+const myNum = [1,2,3,4,5,6,7,8,9,10]
 // const newNum = myNum.map( (num) => num+10)
 //chaining NUm
-//ypu can do n number of chaning
+//you can do n number of chaning
 
 const newNum = myNum
                     .map((num)=>num*10)
@@ -10,4 +32,4 @@ const newNum = myNum
 
 console.log(newNum)
 
-//chaining mehtods
+
