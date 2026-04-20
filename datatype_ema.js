b0ab6734f2cb588698dec2574c10dbb4a0c7ba4a
Dipu <153580850+21tuckdipu@users.noreply.github.com>
2026-04-20 15:13:54 +0530

    updated datatype_ema.js

--- datatype_ema.js
+++ datatype_ema.js
@@ -23,5 +23,30 @@ primitive datetype
 
 
 */
-console.log(typeof null);     //object
-console.log(typeof undefined);  // undefined
+
+let name = "Dipu";
+
+let number = 1;
+let float_number = 1.23;
+let big_int_number = 1n;
+
+let isFalseBoolean = false;
+let isTrueBoolean = true;
+
+let myArr = ['Apple',["Graphs","Banana"],'Mango'];
+let myObj = {"city":"mumbai"};
+
+console.log(`${name} : ${typeof name}`);
+console.log(`${float_number} : ${typeof float_number}`);
+
+console.log(`${big_int_number} : ${typeof big_int_number}`);
+console.log(`${null} : ${typeof null}`);     //object
+console.log(`${undefined} : ${typeof undefined}`);  // undefined
+
+//boolean
+console.log(`${isFalseBoolean} : ${typeof isFalseBoolean}`);
+console.log(`${isTrueBoolean} : ${typeof isTrueBoolean}`);
+
+console.log(`${JSON.stringify(myArr)} : ${typeof myArr}`);
+console.log(`${myArr.join("-")} : ${typeof myArr}`);
+console.log(`${JSON.stringify(myObj)} : ${typeof myObj}`);
