1faddfc761fecf1a33fa76cd38d9ef32660193a2
Dipu <153580850+21tuckdipu@users.noreply.github.com>
2026-04-20 16:37:18 +0530

    updaed and add changes in datatype_conversion.js

--- datatype_conversion.js
+++ datatype_conversion.js
@@ -23,11 +23,39 @@ Boolean Conversion
 0 = zalse
 "dipu" = true
 
-
-
-
 */
+
+let schoolName = "KHS";
+let lowPaySalary = 5000;
+let totalMonthINYears = "12";
+let avgPassCollegePassOut = "22year";
+
+//let see the conversionn effect
+console.log(`${schoolName} Convert to Number: ${Number(schoolName)}`);
+console.log(`${lowPaySalary} Convert to String : ${lowPaySalary.toString()}`);
+console.log(`${totalMonthINYears} Convert TO Number : ${totalMonthINYears}`);
+console.log(`${avgPassCollegePassOut} Convert to number : ${Number(avgPassCollegePassOut)}`);
+
+//conversion of Null
+console.log(`Null Conversion To Number : ${Number(null)}`);
+
+//conversion of undefined
+console.log("Undefined converted to Number : "+Number(undefined));
+
+//Boolean Conversions
+console.log(`True Conversion To Number : ${Number(true)}`);
+console.log(`False Conversion To Number : ${Number(false)}`);
+
+console.log(`0 Conversion To Boolean : ${Boolean(0)}`);
+console.log(`1 Conversion To Boolean : ${Boolean(1)}`);
+
+//string conversion to Boolean
+console.log(`${schoolName} to Boolean : ${Boolean(schoolName)}`);
+
 let isloggin = 1
 
 let booleanIsloggin = Boolean(isloggin)
 console.log(booleanIsloggin);
+
+
+//run command npm run type-conversion
