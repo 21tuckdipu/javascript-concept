3e4d9695f9e92b313a78c48ef25a8d97b21c69f5
Dipu <153580850+21tuckdipu@users.noreply.github.com>
2026-05-04 00:09:55 +0530

    added the reuable functiion

---
+++ reuseable.feature.js
@@ -0,0 +1,7 @@
+export function printDivider(){
+    console.log("---------------------------------");
+}
+
+export function displayHeade(header="hello"){
+    console.log(`===============${header.toUpperCase()}==============`)
+}
